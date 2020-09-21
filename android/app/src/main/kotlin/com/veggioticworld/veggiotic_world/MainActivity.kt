ea4c64d35c1e7679bcc27ab399c57fe3748ee9f1
TheCodingWizard-911 <ganeshs.gitte@gmail.com>
2020-09-21 13:02:35 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/veggioticworld/veggiotic_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.veggioticworld.veggiotic_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(VeggioticWorld());
+}
+
+class VeggioticWorld extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
